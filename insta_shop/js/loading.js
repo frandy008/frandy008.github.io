9e0304ffc33f6c7b4d4b54fe64bef1b8e7d36a34
IM Frandy D Putra <60122275+frandy008@users.noreply.github.com>
2021-10-22 20:29:45 +0800

    new
    Upload page baru

--- insta_shop/js/loading.js
+++
@@ -1,9 +0,0 @@
-$( document ).ajaxStart( function() {
-  $( "#ajax-wait" ).css({
-    left: ( $(window).width() - 32 ) / 2 + "px", // 32 = lebar gambar
-    top: ( $(window).height() - 32 ) / 2 + "px", // 32 = tinggi gambar
-    display: "block"
-  })
-}).ajaxComplete( function() {
-  $( "#ajax-wait" ).fadeOut();
-});
